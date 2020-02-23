e13b120b407a442407a53403c8017eab3316706a
Evren Ermiş <36535914+eermis1@users.noreply.github.com>
2020-02-23 19:01:01 +0300

    View Update / Update, Delete

--- mysite/firstapp/api/urls.py
+++ mysite/firstapp/api/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from firstapp.api.views import api_community_detail_view, api_community_list_view
+from firstapp.api.views import (api_community_detail_view, 
+                                api_community_list_view, 
+                                api_community_update_view, 
+                                api_community_delete_view,
+                                class_api_community_update_view)
 
 app_name = "firstapp"
 
 urlpatterns = [
      path("", api_community_list_view.as_view(), name="all_community_list_view"),
-     path("<int:community_id>", api_community_detail_view, name="community_detail")
+     path("<int:community_id>", api_community_detail_view, name="community_detail"),
+     path("update/<int:community_id>/", api_community_update_view, name="community_update"),
+     path("<int:community_id>/update/", api_community_update_view, name="community_update_class"),
+     path("delete/<int:community_id>/", api_community_delete_view, name="community_delete")
 ]
 
 
--- mysite/firstapp/api/views.py
+++ mysite/firstapp/api/views.py
@@ -1,12 +1,16 @@
 from rest_framework import status 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import (ListAPIView,
+                                     RetrieveAPIView,
+                                     DestroyAPIView,
+                                     UpdateAPIView,
+                                     CreateAPIView,)
  
 from firstapp.models import Community, Post_Type
 from firstapp.api.serializers import CommunitySerializer, Post_TypeSerializer
 
-# List of all communities, LISTAPIVIEW use
+# List of all communities, LISTAPIVIEW use case
 class api_community_list_view(ListAPIView):
     
     serializer_class = CommunitySerializer
@@ -14,8 +18,11 @@ class api_community_list_view(ListAPIView):
         communities = Community.objects.order_by("-community_creation_date")
         return communities
 
-# Detail view of any community, function based view.
-@api_view(["GET"])
+
+# -------------------------------------------------- Detail View --------------------------------------------------------------------
+
+# Function based view.
+@api_view(["GET", ]) # GET is used for list operations
 def api_community_detail_view(request,community_id):
 
     try:
@@ -26,3 +33,69 @@ def api_community_detail_view(request,community_id):
     if request.method == "GET":
         serializer = CommunitySerializer(community)
         return Response(serializer.data)
+
+# Class based view.
+# class api_community_detail_view(RetrieveAPIView):
+#     queryset = Community.objects.all()
+#     serializer_class = CommunitySerializer
+#     lookup_field = 'slug'
+
+
+# -------------------------------------------------- Update View --------------------------------------------------------------------
+
+# Function based view.
+@api_view(["PUT", ]) # PUT is used for update operations
+def api_community_update_view(request,community_id):
+
+    try:
+        community = Community.objects.get(pk = community_id)
+    except Community.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND) 
+
+    if request.method == "PUT":
+        serializer = CommunitySerializer(community, data=request.data)
+        data = {} # Refers to Context in usual views.
+        if serializer.is_valid():
+            serializer.save() 
+            data["success"] = "Update Successful" # We send only Update Successful after update therefore we addedd this.
+            return Response(data=data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Class based view.
+class class_api_community_update_view(UpdateAPIView):
+    queryset = Community.objects.all()
+    serializer_class = CommunitySerializer
+    lookup_field = 'pk'
+
+
+# -------------------------------------------------- Delete View --------------------------------------------------------------------
+
+# Function based view.
+@api_view(["Delete", ]) # DELETE is used for delete operations
+def api_community_delete_view(request,community_id):
+
+    try:
+        community = Community.objects.get(pk = community_id)
+    except Community.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND) 
+
+    if request.method == "DELETE":
+        operation = Community.delete()
+        data = {} # Refers to Context in usual views.
+        if operation:
+            data["success"] = "Update Successful" # We send only Update Successful after update therefore we addedd this.
+        else:
+            data["failure"] = "Update Failed"
+            return Response(data=data)
+        return Response(data=data)
+
+
+
+
+
+
+
+
+
+# -------------------------------------------------- Create View --------------------------------------------------------------------
